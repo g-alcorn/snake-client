0529d4d7cff2b151f7b206a17ee25ec9df4985a0
g-alcorn <griffin.alcorn@gmail.com>
2019-09-25 17:29:47 +0000

    input functions moved to separate file

--- play.js
+++ play.js
@@ -1,26 +1,6 @@
 const net = require('net');
 const { connect } = require('./client');
-
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (key) => {
-    handleUserInput(key);
-  });
-  return stdin;
-}
-
-const handleUserInput = function(input) {
-  if (input === '\u0003')
-    process.exit();
-}
-
+const { setupInput } = require('./input');
 console.log('Connecting ...');
 setupInput();
 connect();
