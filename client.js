50edc8844ec4dc6fdac9917f95b677c5ec2beab4
g-alcorn <griffin.alcorn@gmail.com>
2019-09-25 16:43:19 +0000

    moved connect function to separate client file

---
+++ client.js
@@ -0,0 +1,27 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '192.168.0.103',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('data', (data) => {
+    console.log(`Message from server: ${data}`);
+  });
+
+  return conn;
+}
+
+
+let exporting = {
+  connect
+};
+
+
+module.exports = exporting;
